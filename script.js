5f00ed48a8a5f83c43d3c4ccfdf4fe02947dd855
Leonardo-Peres <128265932+Leonardo-Peres@users.noreply.github.com>
2023-04-03 00:02:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal({ 
+    reset: false,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('#conecte', { delay: 500, origin: 'bottom', interval: 200 });
